d8ec2a84f601c65dbda12db6c08d3b851f9634ff
Alex Manelis <amanelis@gmail.com>
2012-11-26 16:07:16 -0800

    change the method of returning cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,8 @@
 class CatsController < ApplicationController
   def index
     bucket  = AWS::S3::Bucket.find(THERESA_SHULTZ_BUCKET)
-    @images = bucket.objects.collect { |object| AWS::S3::S3Object.url_for(object.key, THERESA_SHULTZ_BUCKET, authenticated: false) }.reject { |x| x.include?('background')}
+    @images = bucket.objects.collect { |object| 
+      AWS::S3::S3Object.url_for(object.key, THERESA_SHULTZ_BUCKET, authenticated: false) }.reject { |x| x.include?('background') || x.include?('folder') 
+    }
   end
 end
