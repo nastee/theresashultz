2f9c86b13fb0f6fc6c6f98bced1f171a76b72b84
Alex Manelis <amanelis@gmail.com>
2013-08-01 19:18:25 -0700

    Some css fun

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,24 +12,14 @@
 //
 // = require jquery
 // = require jquery.masonry.min
+// = require jquery.isotope.min
 // = require jquery.infinitescroll.min
 // = require jquery.lazyload.min
 // = require jquery.lettering
 // = require jquery.textillate
 // = require manual-trigger
-// = require davis.min
 // = require facebox
 // = require blur
-
-/**
- * Instantiate Davis.js
- */
-// var app = Davis(function () {
-//   this.get('/#index', function (req) {
-//   });
-// });
-// 
-// app.start();
 
 /**
  * Top level namespace for Jasmine, a lightweight JavaScript BDD/spec/testing framework.
@@ -42,64 +32,80 @@ var tshultz = {};
 var isCommonJS = typeof window == "undefined";
 if (isCommonJS) exports.tshultz = tshultz;
 
-tshultz.initialize_cats_infinite_scroll = function() {
-  // Access the single container across all jquery
-  var $container = $('#container');
+// tshultz.initialize_portfolio = function() {
+//   var $container = $('#container');
+// 
+//   $container.isotope({
+//     itemSelector : '.item',
+//     layoutMode : 'fitRows'
+//   });
+//   
+//   $container.imagesLoaded(function(){
+//     $container.masonry({
+//       itemSelector : '.item',
+//       columnWidth : 240
+//     });
+//   });
+// };
 
-  // Load the images into Masonry after page completes
-  $container.imagesLoaded(function(){
-    $container.masonry({
-      itemSelector : '.item',
-      columnWidth : 240
-    });
-  });
+// tshultz.initialize_cats_infinite_scroll = function() {
+//   // Access the single container across all jquery
+//   var $container = $('#container');
+// 
+//   // Load the images into Masonry after page completes
+//   $container.imagesLoaded(function(){
+//     $container.masonry({
+//       itemSelector : '.item',
+//       columnWidth : 240
+//     });
+//   });
+// 
+//   $container.infinitescroll({
+//     navSelector  : '#page-nav',     // selector for the paged navigation
+//     nextSelector : '#page-nav a',   // selector for the NEXT link (to page 2)
+//     itemSelector : '.item',         // selector for all items you'll retrieve
+//     debug : false,
+//     loading: {
+//       img: "http://i.imgur.com/qkKy8.gif",
+//       finishedMsg: "No more Images to load.",
+//         img: "http://i.imgur.com/qkKy8.gif",
+//         msgText: "<em>Loading Images...</em>"
+//       },
+//     },
+// 
+//     // trigger Masonry as a callback
+//     function(newElements) {
+//       var $newElems = $(newElements).css({opacity: 0});
+//       $newElems.imagesLoaded(function(){
+//         $newElems.animate({ opacity: 1 });
+//         $container.masonry('appended', $newElems, true,function() {
+//           console.log('completed');
+//         });
+//       });
+//     }
+//   );
+// };
 
-  $container.infinitescroll({
-    navSelector  : '#page-nav',     // selector for the paged navigation
-    nextSelector : '#page-nav a',   // selector for the NEXT link (to page 2)
-    itemSelector : '.item',         // selector for all items you'll retrieve
-    debug : false,
-    loading: {
-      img: "http://i.imgur.com/qkKy8.gif",
-      finishedMsg: "No more Images to load.",
-        img: "http://i.imgur.com/qkKy8.gif",
-        msgText: "<em>Loading Images...</em>"
-      },
-    },
 
-    // trigger Masonry as a callback
-    function(newElements) {
-      var $newElems = $(newElements).css({opacity: 0});
-      $newElems.imagesLoaded(function(){
-        $newElems.animate({ opacity: 1 });
-        $container.masonry('appended', $newElems, true,function() {
-          console.log('completed');
-        });
-      });
-    }
-  );
-};
-
-
-$(document).ready(function() {
-	// Remove the pre load
-	$("window").load(function() {
-	  $("body").removeClass("preload");
-	});
-	
-	// Lazy load the images
-  $("img.item").lazyload({ 
-      effect : "fadeIn"
-  });
-  
-  // Setup click handlers for facebook rels
-  $('a[rel*=facebox]').facebox();
-
-	// Textillate the h1 text
-	$('h1.theresa').textillate({ 
-		in: { effect: 'fadeInDown' } 
-	});
-	
-	// Fade in gravatar
-	$('#gravatar').delay(1000).fadeIn(1000);
-});
+// $(document).ready(function() {
+//  // Remove the pre load
+//  $("window").load(function() {
+//    $("body").removeClass("preload");
+//  });
+//  
+//  // Lazy load the images
+//   $("img.item").lazyload({ 
+//       effect : "fadeIn"
+//   });
+//   
+//   // Setup click handlers for facebook rels
+//   $('a[rel*=facebox]').facebox();
+// 
+//  // Textillate the h1 text
+//  $('h1.theresa').textillate({ 
+//    in: { effect: 'fadeInDown' } 
+//  });
+//  
+//  // Fade in gravatar
+//  $('#gravatar').delay(1000).fadeIn(1000);
+// });
